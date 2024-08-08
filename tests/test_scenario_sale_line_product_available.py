9690eb4a33193305faea2a680328fd0727fc1f2e
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2024-08-08 11:23:29 +0200

    Convert ‘test’ files to .py

---
+++ tests/test_scenario_sale_line_product_available.py
@@ -0,0 +1,198 @@
+import datetime
+import unittest
+from decimal import Decimal
+
+from proteus import Model
+from trytond.modules.account.tests.tools import (create_chart,
+                                                 create_fiscalyear, create_tax,
+                                                 get_accounts)
+from trytond.modules.account_invoice.tests.tools import (
+    create_payment_term, set_fiscalyear_invoice_sequences)
+from trytond.modules.company.tests.tools import create_company, get_company
+from trytond.tests.test_tryton import drop_db
+from trytond.tests.tools import activate_modules
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        drop_db()
+        super().setUp()
+
+    def tearDown(self):
+        drop_db()
+        super().tearDown()
+
+    def test(self):
+
+        # Install account_invoice
+        config = activate_modules('sale_line_product_available')
+
+        # Create company
+        _ = create_company()
+        company = get_company()
+
+        # Reload the context
+        User = Model.get('res.user')
+        Group = Model.get('res.group')
+        config._context = User.get_preferences(True, config.context)
+
+        # Create sale user
+        sale_user = User()
+        sale_user.name = 'Sale'
+        sale_user.login = 'sale'
+        sale_group, = Group.find([('name', '=', 'Sales')])
+        sale_user.groups.append(sale_group)
+        sale_user.save()
+
+        # Create stock user
+        stock_user = User()
+        stock_user.name = 'Stock'
+        stock_user.login = 'stock'
+        stock_group, = Group.find([('name', '=', 'Stock')])
+        stock_user.groups.append(stock_group)
+        stock_user.save()
+
+        # Create account user
+        account_user = User()
+        account_user.name = 'Account'
+        account_user.login = 'account'
+        account_group, = Group.find([('name', '=', 'Account')])
+        account_user.groups.append(account_group)
+        account_user.save()
+
+        # Create fiscal year
+        fiscalyear = set_fiscalyear_invoice_sequences(
+            create_fiscalyear(company))
+        fiscalyear.click('create_period')
+
+        # Create chart of accounts
+        _ = create_chart(company)
+        accounts = get_accounts(company)
+        revenue = accounts['revenue']
+        expense = accounts['expense']
+
+        # Create tax
+        tax = create_tax(Decimal('.10'))
+        tax.save()
+
+        # Create parties
+        Party = Model.get('party.party')
+        supplier = Party(name='Supplier')
+        supplier.save()
+        customer = Party(name='Customer')
+        customer.save()
+
+        # Create account category
+        ProductCategory = Model.get('product.category')
+        account_category = ProductCategory(name="Account Category")
+        account_category.accounting = True
+        account_category.account_expense = expense
+        account_category.account_revenue = revenue
+        account_category.customer_taxes.append(tax)
+        account_category.save()
+
+        # Create product
+        ProductUom = Model.get('product.uom')
+        unit, = ProductUom.find([('name', '=', 'Unit')])
+        ProductTemplate = Model.get('product.template')
+        Product = Model.get('product.product')
+        product = Product()
+        template = ProductTemplate()
+        template.name = 'product'
+        template.default_uom = unit
+        template.type = 'goods'
+        template.salable = True
+        template.list_price = Decimal('10')
+        template.cost_price = Decimal('5')
+        template.cost_price_method = 'fixed'
+        template.account_category = account_category
+        template.save()
+        product, = template.products
+        product.save()
+        service = Product()
+        template = ProductTemplate()
+        template.name = 'service'
+        template.default_uom = unit
+        template.type = 'service'
+        template.salable = True
+        template.list_price = Decimal('30')
+        template.cost_price = Decimal('10')
+        template.cost_price_method = 'fixed'
+        template.account_category = account_category
+        template.save()
+        service, = template.products
+        service.save()
+
+        # Create payment term
+        payment_term = create_payment_term()
+        payment_term.save()
+
+        # Create an Inventory
+        config.user = stock_user.id
+        Inventory = Model.get('stock.inventory')
+        Location = Model.get('stock.location')
+        storage, = Location.find([('code', '=', 'STO')])
+        inventory = Inventory()
+        inventory.location = storage
+        inventory_line = inventory.lines.new(product=product)
+        inventory_line.quantity = 100.0
+        inventory_line.expected_quantity = 0.0
+        inventory.click('confirm')
+        self.assertEqual(inventory.state, 'done')
+
+        # Sale 5 products
+        today = datetime.date.today()
+        config.user = sale_user.id
+        Sale = Model.get('sale.sale')
+        SaleLine = Model.get('sale.line')
+        sale = Sale()
+        sale.party = customer
+        sale.payment_term = payment_term
+        sale.invoice_method = 'order'
+        sale.sale_date = today
+        sale_line = SaleLine()
+        sale.lines.append(sale_line)
+        sale_line.product = product
+        sale_line.quantity = 5
+        self.assertEqual(sale_line.available_quantity, 100.0)
+        self.assertEqual(sale_line.forecast_quantity, 95.0)
+
+        sale_line = SaleLine()
+        sale.lines.append(sale_line)
+        sale_line.type = 'comment'
+        sale_line.description = 'Comment'
+        sale.click('quote')
+        sale.click('confirm')
+        shipment, = sale.shipments
+        sale2 = Sale()
+        sale2.party = customer
+        sale2.payment_term = payment_term
+        sale2.invoice_method = 'order'
+        sale2_line = SaleLine()
+        sale2.lines.append(sale2_line)
+        sale2_line.product = product
+        sale2_line.quantity = 5
+        self.assertEqual(sale2_line.available_quantity, 100.0)
+        self.assertEqual(sale2_line.forecast_quantity, 90.0)
+
+        sale2.save()
+
+        # Done shipment
+        config.user = stock_user.id
+        shipment.click('assign_try')
+        shipment.click('pick')
+        shipment.click('pack')
+        shipment.click('done')
+        self.assertEqual(shipment.state, 'done')
+
+        # Check quantities in sale 2
+        config.user = sale_user.id
+        line2, = sale2.lines
+        self.assertEqual(line2.available_quantity, 95.0)
+        self.assertEqual(line2.forecast_quantity, 90.0)
+
+        sale2.click('quote')
+        sale2.click('confirm')
+        line2.reload()
+        self.assertEqual(line2.forecast_quantity, 90.0)
